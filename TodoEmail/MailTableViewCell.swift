40e6a7f80c43756013c1952012a0627e766c60f6
bmtechua <bmtech@i.ua>
2022-03-01 20:11:52 +0200

    create custom cell

---
+++ TodoEmail/MailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MailTableViewCell.swift
+//  TodoEmail
+//
+//  Created by bmtech on 01.03.2022.
+//
+
+import UIKit
+
+class MailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TodoEmail/ViewController.swift
+++ TodoEmail/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let idCell = "mailCell"
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UINib(nibName: "MailTableViewCell", bundle: nil), forCellReuseIdentifier: idCell)
     }
 
 
 }
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    //метод dataSource додавання секцій (груп)
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    //додавання комірок з різною кількістю у секції
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 2
+        } else {
+            return 4
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: idCell) as! MailTableViewCell
+        
+      
+        return cell
+        }
+    //метод dataSource додавання header section
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section "
+    }
+    //методи delegate додавання висоти комірок через tableView.delegate = self -> UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    //методи delegate вивід після натиснення на комірку didSelectRowAt
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)")
+    }
+    
+    
+}
+
 
